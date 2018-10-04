f0d39482ced4935f913c89c14665fbee385c0323
linliangtao <1428129601@qq.com>
2018-10-04 23:05:05 +0800

    准备样式

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 var result = `
-body{
-    background:#444;
+/* 
+ * 面试官你好，我是林良涛
+ * 只用文字作做我介绍太单调了
+ * 我就用代码来介绍吧
+ * 首先准备一些样式
+ */
+*{
+  transition: all 1s;
+}
+html{
+  background: #eee;
+}
+#code{
+  border: 1px solid #aaa;
+  padding: 16px;
 }
 `
 
