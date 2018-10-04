f62c504ee321ce8493383728ab5fda46547dc7cb
linliangtao <1428129601@qq.com>
2018-10-04 22:03:27 +0800

    使用preview标签解决空格回车问题

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-var result = '1234567890'
+var result = `
+body{
+    background:blue;
+}
+`
+
+
 
 var n = 0
 var id = setInterval(()=>{
 
     n += 1
-    document.body.innerHTML = result.substring(0,n)
+    code.innerHTML = result.substring(0,n)
     console.log('一轮')
     if(n >= result.length){
         window.clearInterval(id)
