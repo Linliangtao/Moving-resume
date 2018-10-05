8f106ef8309b4d13e7160681858e701a4015814a
linliangtao <1428129601@qq.com>
2018-10-05 15:12:50 +0800

    加点3D效果

--- js/main.js
+++ js/main.js
@@ -4,27 +4,45 @@ var result = `/*
  * 我就用代码来介绍吧
  * 首先准备一些样式
  */
+/* 首先给所有元素加上过渡效果 */
 *{
   transition: all 1s;
 }
+/* 背景太单调，我们加点背景*/
 html{
-  background: rgb(222,222,222);
-  font-size:16px;
+  color: rgb(222,222,222); background: rgb(0,43,54);
 }
 #code{
   border: 1px solid #aaa;
   padding: 16px;
 }
+/* 文字离边框太近了 */
+ #code {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+}
 /* 我需要一点代码高亮 */
-.token.selector{
-  color:#690;
+.token.selector{ color: rgb(133,153,0); }
+.token.property{ color: rgb(187,137,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(42,161,152); }
+
+
+/* 加点 3D 效果呗 */
+html{
+  perspective: 1000px;
 }
-.token.property{
-  color:#905;
+#code {
+  position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
 }
-.token.function{
-  color:#DD4A68;
-}
+
 `
 
 
@@ -40,4 +58,4 @@ var id = setInterval(()=>{
     if(n >= result.length){
         window.clearInterval(id)
     }
-},20)
+},10)
