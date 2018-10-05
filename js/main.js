51e531349f02c2fad0fd7509c4326a24e5610231
linliangtao <1428129601@qq.com>
2018-10-05 18:35:58 +0800

    自我介绍

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-var result = `/* 
+
+function writeCss(prefix, code, fn){
+  let domCode = document.querySelector('#code')
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+    domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
+    styleTag.innerHTML = prefix +  code.substring(0, n)
+    domCode.scrollTop = domCode.scrollHeight
+    if (n >= code.length) {
+      window.clearInterval(id)
+      fn && fn.call()
+    }
+  }, 70)
+}
+function writeMarkdown(markdown, fn){
+  let domPaper = document.querySelector('#paper>.content')
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+    domPaper.innerHTML = markdown.substring(0, n)
+    domPaper.scrollTop = domPaper.scrollHeight
+    if (n >= markdown.length) {
+      window.clearInterval(id)
+      fn && fn.call()
+    }
+  }, 10)
+}
+
+var css1 = `/* 
  * 面试官你好，我是林良涛
  * 只用文字作做自我介绍太单调了
  * 我就用代码来介绍吧
@@ -52,12 +81,6 @@ html{
 /* 好了，我来介绍我自己吧 */
 /* 我需要一张白纸 */
 #paper{
-  width: 100px;
-  height:100px;
-  background:red;
-}
-/**/
-#paper{
   position: fixed; right: 0; top: 0;
   padding: .5em;  margin: .5em;
   width: 48vw; height: 90vh;
@@ -65,46 +88,72 @@ html{
   background: white; color: #222;
   overflow: auto;
 }
-}
+/* 于是我就可以在白纸上写字了，请看右边 */
+`
+
+var css2 = `
+/* 接下来用一个优秀的库 marked.js
+ * 把 Markdown 变成 HTML
+ */
+`
+var md = `
+# 自我介绍
+我叫 XXX
+1990 年 1 月出生
+XXX 学校毕业
+自学前端半年
+希望应聘前端开发岗位
+# 技能介绍
+熟悉 JavaScript CSS
+# 项目介绍
+1. XXX 轮播
+2. XXX 简历
+3. XXX 画板
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+`
+let css3 = `
+/*
+ * 这就是我的会动的简历
+ * 谢谢观看
+ */
+
 `
 
 
 
-var n = 0
-var id = setInterval(()=>{
+writeCss('', css1, ()=>{ // writeCss call the function
+  createPaper(() => {
+    writeMarkdown(md, ()=> {
+      writeCss(css1, css2, ()=>{
+        convertMarkdownToHtml(()=>{
+          writeCss(css1 + css2, css3, ()=> {
+            console.log('完成')
+          })
+        })
+      })
+    })
+  })
+})
 
-    n += 1
-    code.innerHTML = result.substring(0,n)
-    code.innerHTML =
-     Prism.highlight(code.innerHTML, Prism.languages.css);
-    styleTag.innerHTML = result.substring(0,n)
-    if(n >= result.length){
-        window.clearInterval(id)
-        fn2()
-        fn3()
-    }
-},50)
-
-function fn2(){
-  var paper = document.createElement('div')
+function createPaper(fn){
+  var paper = document.createElement('div') 
   paper.id = 'paper'
+  var content = document.createElement('pre')
+  content.className = 'content'
+  paper.appendChild(content)
   document.body.appendChild(paper)
+  fn && fn.call()
 }
-function fn3(preResult){
-  var result = `
-  #paper{
-    width: 100px;height:100px;
-    background:red;
-  }
-  `
-  var n =0
-  var id = setInterval(()=>{
-    n += 1
-    code.innerHTML = preResult + preresult.substring(0,n)
-    code.innerHTML =
-     Prism.highlight(code.innerHTML, Prism.languages.css);
-    if(n >= result.length){
-      window.clearInterval(id)
-    }
-  },50)
+
+function convertMarkdownToHtml(fn){
+  var div = document.createElement('div')  
+  div.className = 'html markdown-body'
+  div.innerHTML = marked(md)
+  let markdownContainer = document.querySelector('#paper > .content')
+  markdownContainer.replaceWith(div)
+  fn && fn.call()
 }
