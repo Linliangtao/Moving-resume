83f5654d19f4b06bf54e031e2f06120d8fe5183a
linliangtao <1428129601@qq.com>
2018-10-05 14:24:20 +0800

    高亮代码

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var result = `
-/* 
+var text = "body{color:red;}"
+
+
+var result = `/* 
  * 面试官你好，我是林良涛
- * 只用文字作做我介绍太单调了
+ * 只用文字作做自我介绍太单调了
  * 我就用代码来介绍吧
  * 首先准备一些样式
  */
@@ -9,7 +11,8 @@ var result = `
   transition: all 1s;
 }
 html{
-  background: #eee;
+  background: rgb(222,222,222);
+  font-size:16px;
 }
 #code{
   border: 1px solid #aaa;
@@ -24,9 +27,10 @@ var id = setInterval(()=>{
 
     n += 1
     code.innerHTML = result.substring(0,n)
+    code.innerHTML =
+     Prism.highlight(code.innerHTML, Prism.languages.css);
     styleTag.innerHTML = result.substring(0,n)
-    console.log('一轮')
     if(n >= result.length){
         window.clearInterval(id)
     }
-},100)
+},10)
