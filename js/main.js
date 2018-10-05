f0876d421298f19e3557b4f2cfa3c8637f713d33
linliangtao <1428129601@qq.com>
2018-10-05 16:01:42 +0800

    加个白纸

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ html{
 .token.function{ color: rgb(42,161,152); }
 
 
+
 /* 加点 3D 效果呗 */
 html{
   perspective: 1000px;
@@ -41,6 +42,15 @@ html{
   transition: none;
   -webkit-transform: rotateY(10deg) translateZ(-100px) ;
           transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+
+/* 好了，我来介绍我自己吧 */
+/* 我需要一张白纸 */
+#paper{
+  width: 100px;
+  height:100px;
+  background:red;
 }
 
 `
@@ -57,5 +67,31 @@ var id = setInterval(()=>{
     styleTag.innerHTML = result.substring(0,n)
     if(n >= result.length){
         window.clearInterval(id)
+        fn2()
+        fn3()
     }
 },10)
+
+function fn2(){
+  var paper = document.createElement('div')
+  paper.id = 'paper'
+  document.body.appendChild(paper)
+}
+function fn3(preResult){
+  var result = `
+  #paper{
+    width: 100px;height:100px;
+    background:red;
+  }
+  `
+  var n =0
+  var id = setInterval(()=>{
+    n += 1
+    code.innerHTML = preResult + preresult.substring(0,n)
+    code.innerHTML =
+     Prism.highlight(code.innerHTML, Prism.languages.css);
+    if(n >= result.length){
+      window.clearInterval(id)
+    }
+  },10)
+}
